5be8c91aa533d84679f7335d49f279193709dade
mburly <mburleson@cfl.rr.com>
2023-03-01 11:24:46 -0500

    add functionality to pull building names, descriptions, and pictures from database, move dropdown javascript to js file

---
+++ ETSU.CampusTour.WebApp/Controllers/BuildingController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using System.Net.Http;
+using Newtonsoft.Json;
+using ETSU.CampusTour.SharedLibrary.Models;
+
+namespace ETSU.CampusTour.WebAPI.Controllers
+{
+    public class BuildingController : Controller
+    {
+        private readonly HttpClient _httpClient;
+        private readonly string _apiUrl = "https://localhost:7246/api/Building";
+
+        public BuildingController(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        // GET: Building
+        public async Task<IActionResult> Index()
+        {
+            var response = await _httpClient.GetAsync(_apiUrl);
+            if (response.IsSuccessStatusCode)
+            {
+                var buildingsJson = await response.Content.ReadAsStringAsync();
+                var buildings = JsonConvert.DeserializeObject<List<Building>>(buildingsJson);
+                return View(buildings);
+            }
+
+            return BadRequest();
+        }
+
+        // GET: Building/Details/5
+        public async Task<IActionResult> Details(int id)
+        {
+            var response = await _httpClient.GetAsync($"{_apiUrl}/{id}");
+            if (response.IsSuccessStatusCode)
+            {
+                var buildingJson = await response.Content.ReadAsStringAsync();
+                var building = JsonConvert.DeserializeObject<Building>(buildingJson);
+                return View(building);
+            }
+
+            return NotFound();
+        }
+
+    }
+}
